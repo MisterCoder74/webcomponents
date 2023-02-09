b3e8a611f5689316f226edbe72711b461b3e9d80
MisterCoder74 <89546318+MisterCoder74@users.noreply.github.com>
2023-02-09 19:04:34 +0100

    major update
    major modification to files, deletion of image instructions, modified html codes

--- imagecard/imagecard.js
+++ imagecard/imagecard.js
@@ -3,7 +3,7 @@ constructor() {
 super();
 
 var hcustomContent = this.innerHTML;
-this.innerHTML = `<div class=imagecard style=\"background-image: url(${this.getAttribute('image')}); --bd-color: ${this.getAttribute('bordercolor')};\"><div id=cardtitle style=\"--tx-color: ${this.getAttribute('textcolor')};  --tx-justify: ${this.getAttribute('align')};\"><b>${this.getAttribute('title')}</b></div> <div id=cardbody style=\"--tx-color: ${this.getAttribute('textcolor')}; --tx-justify: ${this.getAttribute('align')};\"><p>` + hcustomContent + `</p></div></div>`;
+this.innerHTML = `<div class=imagecard style=\"background-image: url(${this.getAttribute('image')}); --bd-color: ${this.getAttribute('bordercolor')};\"><div id=cardtitle style=\"--tx-color: ${this.getAttribute('textcolor')};  --tx-justify: ${this.getAttribute('align')};\"><b>${this.getAttribute('title')}</b></div> <div id=cardbody style=\"--tx-color: ${this.getAttribute('textcolor')}; --tx-justify: ${this.getAttribute('textalign')};\"><p>` + hcustomContent + `</p></div></div>`;
 }
 }
 
--- productcard/productcard.js
+++ productcard/productcard.js
@@ -3,7 +3,7 @@ constructor() {
 super();
 
 var hcustomContent = this.innerHTML;
-this.innerHTML = `<div class=productcard style=\"--bg-color: ${this.getAttribute('bgcolor')}; --bd-color: ${this.getAttribute('bordercolor')};\"><div id=cardimage style=\" --bd-color: ${this.getAttribute('bordercolor')};\"><img src= ${this.getAttribute('image')} /></div><div id=cardtitle style=\"--tx-color: ${this.getAttribute('textcolor')};  --tx-justify: ${this.getAttribute('align')};\"><b>${this.getAttribute('productname')}</b></div> <div id=cardbody style=\"--tx-color: ${this.getAttribute('textcolor')}; --tx-justify: ${this.getAttribute('align')};\"><p>` + hcustomContent + `</p></div></div>`;
+this.innerHTML = `<div class=productcard style=\"--bg-color: ${this.getAttribute('bgcolor')}; --bd-color: ${this.getAttribute('bordercolor')};\"><div id=cardimage style=\" --bd-color: ${this.getAttribute('bordercolor')};\"><img src= ${this.getAttribute('image')} /></div><div id=cardtitle style=\"--tx-color: ${this.getAttribute('textcolor')};  --tx-justify: ${this.getAttribute('textalign')};\"><b>${this.getAttribute('productname')}</b></div> <div id=cardbody style=\"--tx-color: ${this.getAttribute('textcolor')}; --tx-justify: ${this.getAttribute('align')};\"><p>` + hcustomContent + `</p></div></div>`;
 }
 }
 
